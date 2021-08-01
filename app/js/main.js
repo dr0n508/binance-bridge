300feada1da6e5b6861f48a0843c2806da333d2b
dr0n <kornienko.andriy@gmail.com>
2021-08-02 00:00:54 +0300

    add filter

--- app/js/main.js
+++ app/js/main.js
@@ -54,7 +54,25 @@ $(document).ready(function() {
 
     //filter
 
+    $('#filterAsset').on('input', updateFilter);
 
+    function updateFilter(){
+
+        var fullTitle = $('#filterAsset').val();
+        searchItem(fullTitle);
+    }
+
+    function searchItem(fullTitle) {
+        $(".filter-item").each(function() {
+
+            if ($(this).data('filter').search(new RegExp(fullTitle, "i")) < 0
+                ) {
+                $(this).css( "display", "none" );
+            } else {
+                $(this).css( "display", "flex" );
+            }
+        });
+    }
 
 
 
