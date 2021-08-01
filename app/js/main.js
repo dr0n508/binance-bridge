97fdd6f61abf97a60b20a1cbc0269ea879b3e2e9
dr0n <kornienko.andriy@gmail.com>
2021-08-01 19:17:41 +0300

    dropdown

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,38 @@ $(document).ready(function() {
     // });
 
 
-    $(".btn-dropdown").on("click", function(e) {
-        $(this).closest('.selectBox').toggleClass("show");
-        var dropdownItem = $(this).closest('.selectBox');
-        var container = $(this).find(".selectBox__value");
-        container.text(dropdownItem.text);
+    // $(".btn-dropdown").on("click", function(e) {
+    //     $(this).closest('.selectBox').toggleClass("show");
+    //
+    //     var dropdownItem = e.target;
+    //     console.log(dropdownItem);
+    //     // var dropdownItem = $(this).closest('.selectBox');
+    //     // var container = $(this).closest('.selectBox__value');
+    //
+    //
+    //
+    //     // container.text(dropdownItem.text);
+    //     // $(dropdownItem)
+    //     //     .addClass("active")
+    //     //     .siblings()
+    //     //     .removeClass("active");
+    // });
+
+    $(".selectBox").on("click", function(e) {
+        $(this).toggleClass("show");
+
+        var dropdownItem = e.target;
+
+
+
+        console.log(dropdownItem.closest('.network-box'));
+        // var dropdownItem = $(this).closest('.selectBox');
+        var container = $(this).closest('.selectBox').find('.selectBox__value');
+
+        console.log(container);
+
+
+        // container.text(dropdownItem.text);
         $(dropdownItem)
             .addClass("active")
             .siblings()
