b1ea3b5df9ccecbaeaf037758e992921002c313b
dr0n <kornienko.andriy@gmail.com>
2021-08-01 16:32:55 +0300

    ass some html

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 
+    $(".selectBox").on("click", function(e) {
+        $(this).toggleClass("show");
+        var dropdownItem = e.target;
+        var container = $(this).find(".selectBox__value");
+        container.text(dropdownItem.text);
+        $(dropdownItem)
+            .addClass("active")
+            .siblings()
+            .removeClass("active");
+    });
+
+
 
 });
