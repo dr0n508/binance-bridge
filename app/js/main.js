8cdfc6729563d61eec467b35bf318cc40061bc41
dr0n <kornienko.andriy@gmail.com>
2021-08-01 18:37:48 +0300

    ass some html

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
 
-    $(".selectBox").on("click", function(e) {
-        $(this).toggleClass("show");
-        var dropdownItem = e.target;
+    // $(".selectBox").on("click", function(e) {
+    //     $(this).toggleClass("show");
+    //     var dropdownItem = e.target;
+    //     var container = $(this).find(".selectBox__value");
+    //     container.text(dropdownItem.text);
+    //     $(dropdownItem)
+    //         .addClass("active")
+    //         .siblings()
+    //         .removeClass("active");
+    // });
+
+
+    $(".btn-dropdown").on("click", function(e) {
+        $(this).closest('.selectBox').toggleClass("show");
+        var dropdownItem = $(this).closest('.selectBox');
         var container = $(this).find(".selectBox__value");
         container.text(dropdownItem.text);
         $(dropdownItem)
@@ -10,6 +22,7 @@ $(document).ready(function() {
             .siblings()
             .removeClass("active");
     });
+
 
 
 
