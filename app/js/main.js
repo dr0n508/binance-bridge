ff21a48d6170f1beec0b7ec060f1cbd1bfa44b0e
dr0n <kornienko.andriy@gmail.com>
2021-08-02 10:18:18 +0300

    some refactor

--- app/js/main.js
+++ app/js/main.js
@@ -1,59 +1,12 @@
 $(document).ready(function() {
 
-    // $(".selectBox").on("click", function(e) {
-    //     $(this).toggleClass("show");
-    //     var dropdownItem = e.target;
-    //     var container = $(this).find(".selectBox__value");
-    //     container.text(dropdownItem.text);
-    //     $(dropdownItem)
-    //         .addClass("active")
-    //         .siblings()
-    //         .removeClass("active");
-    // });
-
-
-    // $(".btn-dropdown").on("click", function(e) {
-    //     $(this).closest('.selectBox').toggleClass("show");
-    //
-    //     var dropdownItem = e.target;
-    //     console.log(dropdownItem);
-    //     // var dropdownItem = $(this).closest('.selectBox');
-    //     // var container = $(this).closest('.selectBox__value');
-    //
-    //
-    //
-    //     // container.text(dropdownItem.text);
-    //     // $(dropdownItem)
-    //     //     .addClass("active")
-    //     //     .siblings()
-    //     //     .removeClass("active");
-    // });
-
     $(".selectBox").on("click", function(e) {
+
         $(this).toggleClass("show");
 
-        var dropdownItem = e.target;
-
-
-
-        console.log(dropdownItem.closest('.network-box'));
-        // var dropdownItem = $(this).closest('.selectBox');
-        var container = $(this).closest('.selectBox').find('.selectBox__value');
-
-        console.log(container);
-
-
-        // container.text(dropdownItem.text);
-        $(dropdownItem)
-            .addClass("active")
-            .siblings()
-            .removeClass("active");
     });
 
-
-
     //filter
-
     $('#filterAsset').on('input', updateFilter);
 
     function updateFilter(){
@@ -73,8 +26,5 @@ $(document).ready(function() {
             }
         });
     }
-
-
-
 
 });
